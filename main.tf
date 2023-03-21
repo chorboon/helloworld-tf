07e0fa46c5ce1bafc111b90124d35e7e29cbf8c2
James Tan <chorboon@gmail.com>
2023-03-21 17:08:58 +0800

    working with LB

--- main.tf
+++ main.tf
@@ -15,30 +15,8 @@ provider "aws" {
 }
 
 locals {
-  app_port = 22
+  app_port = 80
 }
-
-#variable "name" {
-#  type = string
-#}
-# ------------------------------------------------------------------
-# VPC
-# ------------------------------------------------------------------
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "~> 3.0"
-
-#   name = "HelloWorldVPC"
-
-#   cidr = "10.0.0.0/16"
-
-#   azs             = ["us-east-1a"]
-#   public_subnets  = ["10.0.1.0/24"]
-#   private_subnets = ["10.0.21.0/24"]
-
-#   enable_nat_gateway = true
-# }
-
 resource "aws_vpc" "HelloWorld_vpc_1" {
   cidr_block = "10.0.0.0/16"
 
@@ -47,27 +25,67 @@ resource "aws_vpc" "HelloWorld_vpc_1" {
 
 
 }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.HelloWorld_vpc_1.id
+}
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.HelloWorld_vpc_1.id
 
-resource "aws_subnet" "web_subnet" {
-  vpc_id            = aws_vpc.HelloWorld_vpc_1.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+}
+resource "aws_subnet" "web_subnet-1" {
+  vpc_id                  = aws_vpc.HelloWorld_vpc_1.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "tf-example"
+    Name = "HelloWorld_vpc_1"
   }
 }
-resource "aws_security_group" "hello_world-22-sg" {
-  name = "hello_world-ec2-sg"
+resource "aws_subnet" "web_subnet-2" {
+  vpc_id                  = aws_vpc.HelloWorld_vpc_1.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "web_subnet-3" {
+  vpc_id                  = aws_vpc.HelloWorld_vpc_1.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.HelloWorld_vpc_1.id
+  route_table_id = aws_route_table.example.id
+}
+
+
+resource "aws_security_group" "hello_world-sg" {
+  name   = "hello_world-sg"
+  vpc_id = aws_vpc.HelloWorld_vpc_1.id
 
   ingress {
     description = "Allow Inbound to 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+ #   cidr_blocks = ["156.249.11.137/32"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Inbound to 80"
     from_port   = local.app_port
     to_port     = local.app_port
     protocol    = "tcp"
-    cidr_blocks = ["156.249.11.137/32"]
+    #   cidr_blocks = ["156.249.11.137/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
   #Outgoing traffic
   egress {
     from_port   = 0
@@ -78,37 +96,86 @@ resource "aws_security_group" "hello_world-22-sg" {
 
 }
 
-resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+resource "aws_security_group" "lb_sg" {
+  name   = "lb_sg"
+  vpc_id = aws_vpc.HelloWorld_vpc_1.id
 
-resource "aws_key_pair" "kp" {
-  key_name   = "HelloWorldKey" # Create "myKey" to AWS
-  public_key = tls_private_key.pk.public_key_openssh
-
-  provisioner "local-exec" { # Create "HelloWorldKey.pem" to your computer
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./HelloWorldKey.pem"
+  ingress {
+    description = "Allow Inbound to 80"
+    from_port   = local.app_port
+    to_port     = local.app_port
+    protocol    = "tcp"
+    #   cidr_blocks = ["156.249.11.137/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  #Outgoing traffic
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 resource "aws_key_pair" "ssh-key" {
   key_name   = "ssh-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7MWvd+EGmqASrpQGI+Wl7/JYntkYJ91c/7SkQ0rHyUt84NTR8VX/lKHm5yjvJ+JdfIlou0GSz+zv9cRZn+wi83zjah6tB+vLg5WSoYpkhs/CNixsmkvxgt55XajFa+R4rKS7lAChM5teTKM83u9ccQxQYKcOZCx6B2urZ/IuvIYWoY/zrY21itCPk28KQ2Lx9wtEGTh2GJBda1YCjpOl7zN2Ds/XYFKGfcL20B2y2fBoVpoNtMQJhN0NrKTV0iAEivATvUod/Q66n91FDSB+EVg0ECU6+yZ/GPwEY/1obVD+SFqYKuGjwF7GIec+8T1tWSjQS6wda+DmJf10r1VNjacfxCCXXsvqVPIC4glq3a3OjmWIjGoQ1FwNr+t9Uv7davV/0eUEAWUmunG813PwLSvNEkp6+EJGAUu1f3z8qKPuT7/3gKuGg88ny/YY2Gu2MQhVOEgpOtzhm/ctRtLXxPYoHJ5PfnN+C0VfnfO9j0Y6DE2fwlWoG9gKdPkvMiMc= chorboon@fedora"
 }
 
+resource "aws_lb" "front_end" {
+  name               = "front-end"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.web_subnet-1.id,aws_subnet.web_subnet-2.id,aws_subnet.web_subnet-3.id]
+
+  enable_deletion_protection = false
+
+  #access_logs {
+  #  bucket  = aws_s3_bucket.lb_logs.id
+  #  prefix  = "test-lb"
+  #  enabled = true
+  #}
+
+  #tags = {
+  #  Environment = "production"
+  #}
+}
+
+resource "aws_lb_target_group" "front_end" {
+  name     = "front-end-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.HelloWorld_vpc_1.id
+}
+resource "aws_lb_target_group_attachment" "front_end" {
+  target_group_arn = aws_lb_target_group.front_end.arn
+  target_id        = aws_instance.hello_world-1.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
 resource "aws_instance" "hello_world-1" {
-  ami             = "ami-02f3f602d23f1659d"
-  instance_type   = "t2.micro"
-  key_name        = "ssh-key"
-  security_groups = ["hello_world-22-sg"]
-  subnet_id       = aws_subnet.web_subnet.id
+  ami                    = "ami-02f3f602d23f1659d"
+  instance_type          = "t2.micro"
+  key_name               = "ssh-key"
+  vpc_security_group_ids = [aws_security_group.hello_world-sg.id]
+  subnet_id              = aws_subnet.web_subnet-1.id
 
   provisioner "remote-exec" {
 
     connection {
-      type = "ssh"
-      user = "ec2-user"
-      #      certificate = file("HelloWorldKey.pem")
+      type        = "ssh"
+      user        = "ec2-user"
       private_key = file("~/.ssh/id_rsa2")
       host        = self.public_ip
     }
