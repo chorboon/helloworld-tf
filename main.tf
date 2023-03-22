9f2aee8b408a3de90d5f22ad97f524698471c724
James Tan <chorboon@gmail.com>
2023-03-22 17:47:03 +0800

    Removed public IP and added nat gateway

--- main.tf
+++ main.tf
@@ -25,11 +25,40 @@ resource "aws_vpc" "HelloWorld_vpc_1" {
 
 
 }
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
+
+resource "aws_nat_gateway" "nat_gateway" {
+  subnet_id   = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.nat_gateway.id
+}
+resource "aws_route_table" "web" {
+  vpc_id = aws_vpc.HelloWorld_vpc_1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
+resource "aws_route_table_association" "web-1" {
+  subnet_id = aws_subnet.web_subnet-1.id
+  route_table_id = aws_route_table.web.id
+}
+resource "aws_route_table_association" "web-2" {
+  subnet_id = aws_subnet.web_subnet-2.id
+  route_table_id = aws_route_table.web.id
+}
+resource "aws_route_table_association" "web-3" {
+  subnet_id = aws_subnet.web_subnet-3.id
+  route_table_id = aws_route_table.web.id
+}
+
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.HelloWorld_vpc_1.id
 }
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.HelloWorld_vpc_1.id
+resource "aws_default_route_table" "lb" {
+  default_route_table_id = aws_vpc.HelloWorld_vpc_1.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -37,30 +66,39 @@ resource "aws_route_table" "example" {
   }
 
 }
-resource "aws_subnet" "web_subnet-1" {
+resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.HelloWorld_vpc_1.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
+#  map_public_ip_on_launch = true
 
 }
-resource "aws_subnet" "web_subnet-2" {
+
+resource "aws_subnet" "web_subnet-1" {
   vpc_id            = aws_vpc.HelloWorld_vpc_1.id
   cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+#  map_public_ip_on_launch = true
+
+}
+
+resource "aws_subnet" "web_subnet-2" {
+  vpc_id            = aws_vpc.HelloWorld_vpc_1.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1c"
-  map_public_ip_on_launch = true
+#  map_public_ip_on_launch = true
 }
 resource "aws_subnet" "web_subnet-3" {
   vpc_id            = aws_vpc.HelloWorld_vpc_1.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1b"
   #  map_public_ip_on_launch = true
 }
 
-resource "aws_main_route_table_association" "a" {
-  vpc_id         = aws_vpc.HelloWorld_vpc_1.id
-  route_table_id = aws_route_table.example.id
-}
+#resource "aws_main_route_table_association" "a" {
+#  vpc_id         = aws_vpc.HelloWorld_vpc_1.id
+#  route_table_id = aws_route_table.lb.id
+#}
 
 
 # resource "aws_security_group" "hello_world-sg" {
@@ -97,14 +135,14 @@ resource "aws_security_group" "lb_sg" {
   name   = "lb_sg"
   vpc_id = aws_vpc.HelloWorld_vpc_1.id
 
-  ingress {
-    description = "Allow Inbound to 22"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["156.249.11.137/32"]
- #   cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Allow Inbound to 22"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#  #   cidr_blocks = ["156.249.11.137/32"]
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
   ingress {
     description = "Allow Inbound to 80"
     from_port   = local.app_port
@@ -122,10 +160,10 @@ resource "aws_security_group" "lb_sg" {
   }
 
 }
-resource "aws_key_pair" "ssh-key" {
- key_name   = "ssh-key"
- public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7MWvd+EGmqASrpQGI+Wl7/JYntkYJ91c/7SkQ0rHyUt84NTR8VX/lKHm5yjvJ+JdfIlou0GSz+zv9cRZn+wi83zjah6tB+vLg5WSoYpkhs/CNixsmkvxgt55XajFa+R4rKS7lAChM5teTKM83u9ccQxQYKcOZCx6B2urZ/IuvIYWoY/zrY21itCPk28KQ2Lx9wtEGTh2GJBda1YCjpOl7zN2Ds/XYFKGfcL20B2y2fBoVpoNtMQJhN0NrKTV0iAEivATvUod/Q66n91FDSB+EVg0ECU6+yZ/GPwEY/1obVD+SFqYKuGjwF7GIec+8T1tWSjQS6wda+DmJf10r1VNjacfxCCXXsvqVPIC4glq3a3OjmWIjGoQ1FwNr+t9Uv7davV/0eUEAWUmunG813PwLSvNEkp6+EJGAUu1f3z8qKPuT7/3gKuGg88ny/YY2Gu2MQhVOEgpOtzhm/ctRtLXxPYoHJ5PfnN+C0VfnfO9j0Y6DE2fwlWoG9gKdPkvMiMc= chorboon@fedora"
-}
+# resource "aws_key_pair" "ssh-key" {
+#  key_name   = "ssh-key"
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7MWvd+EGmqASrpQGI+Wl7/JYntkYJ91c/7SkQ0rHyUt84NTR8VX/lKHm5yjvJ+JdfIlou0GSz+zv9cRZn+wi83zjah6tB+vLg5WSoYpkhs/CNixsmkvxgt55XajFa+R4rKS7lAChM5teTKM83u9ccQxQYKcOZCx6B2urZ/IuvIYWoY/zrY21itCPk28KQ2Lx9wtEGTh2GJBda1YCjpOl7zN2Ds/XYFKGfcL20B2y2fBoVpoNtMQJhN0NrKTV0iAEivATvUod/Q66n91FDSB+EVg0ECU6+yZ/GPwEY/1obVD+SFqYKuGjwF7GIec+8T1tWSjQS6wda+DmJf10r1VNjacfxCCXXsvqVPIC4glq3a3OjmWIjGoQ1FwNr+t9Uv7davV/0eUEAWUmunG813PwLSvNEkp6+EJGAUu1f3z8qKPuT7/3gKuGg88ny/YY2Gu2MQhVOEgpOtzhm/ctRtLXxPYoHJ5PfnN+C0VfnfO9j0Y6DE2fwlWoG9gKdPkvMiMc= chorboon@fedora"
+# }
 
 resource "aws_lb" "front_end" {
   name               = "front-end"
@@ -179,8 +217,8 @@ resource "aws_launch_configuration" "web" {
   security_groups      = [aws_security_group.lb_sg.id]
   enable_monitoring    = true
   iam_instance_profile = aws_iam_instance_profile.dev-resources-iam-profile.name
-  associate_public_ip_address = true
-  key_name               = "ssh-key"
+#  associate_public_ip_address = true
+#  key_name               = "ssh-key"
   lifecycle {
     create_before_destroy = true
   }
--- ssm.tf
+++ ssm.tf
@@ -25,22 +25,4 @@ resource "aws_iam_role" "ssm-role" {
 resource "aws_iam_role_policy_attachment" "dev-resources-ssm-policy" {
   role       = aws_iam_role.ssm-role.id
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
-
-# resource "aws_ssm_activation" "web" {
-#   name               = "test_ssm_activation"
-#   description        = "Test"
-#   iam_role           = aws_iam_role.ssm-role.id
-#   registration_limit = "5"
-#   depends_on         = [aws_iam_role_policy_attachment.dev-resources-ssm-policy]
-# }
-
-# resource "aws_ssm_association" "example" {
-#   name = "AmazonCloudWatch-ManageAgent"
-
-#   targets {
-#     key    = "InstanceIds"
-#     values = ["*"]
-#   }
-# }
