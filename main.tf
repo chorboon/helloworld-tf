e7fde31a85fb1f2b9f90b995765ff2fd0f6f5919
James Tan <chorboon@gmail.com>
2023-03-23 13:16:44 +0800

    working lb again

--- main.tf
+++ main.tf
@@ -25,13 +25,21 @@ resource "aws_vpc" "HelloWorld_vpc_1" {
 
 
 }
+
+resource "aws_eip" "frontend-lb-1" {
+  vpc = true
+}
+resource "aws_eip" "frontend-lb-2" {
+  vpc = true
+}
+
 resource "aws_eip" "nat_gateway" {
   vpc = true
 }
 
 
 resource "aws_nat_gateway" "nat_gateway" {
-  subnet_id   = aws_subnet.public_subnet.id
+  subnet_id   = aws_subnet.public_subnet-1.id
   allocation_id = aws_eip.nat_gateway.id
 }
 resource "aws_route_table" "web" {
@@ -66,14 +74,20 @@ resource "aws_default_route_table" "lb" {
   }
 
 }
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet-1" {
   vpc_id            = aws_vpc.HelloWorld_vpc_1.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.10.0/24"
   availability_zone = "us-east-1a"
 #  map_public_ip_on_launch = true
 
 }
+resource "aws_subnet" "public_subnet-2" {
+  vpc_id            = aws_vpc.HelloWorld_vpc_1.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "us-east-1b"
+#  map_public_ip_on_launch = true
 
+}
 resource "aws_subnet" "web_subnet-1" {
   vpc_id            = aws_vpc.HelloWorld_vpc_1.id
   cidr_block        = "10.0.1.0/24"
@@ -160,43 +174,60 @@ resource "aws_security_group" "lb_sg" {
   }
 
 }
-# resource "aws_key_pair" "ssh-key" {
-#  key_name   = "ssh-key"
-#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7MWvd+EGmqASrpQGI+Wl7/JYntkYJ91c/7SkQ0rHyUt84NTR8VX/lKHm5yjvJ+JdfIlou0GSz+zv9cRZn+wi83zjah6tB+vLg5WSoYpkhs/CNixsmkvxgt55XajFa+R4rKS7lAChM5teTKM83u9ccQxQYKcOZCx6B2urZ/IuvIYWoY/zrY21itCPk28KQ2Lx9wtEGTh2GJBda1YCjpOl7zN2Ds/XYFKGfcL20B2y2fBoVpoNtMQJhN0NrKTV0iAEivATvUod/Q66n91FDSB+EVg0ECU6+yZ/GPwEY/1obVD+SFqYKuGjwF7GIec+8T1tWSjQS6wda+DmJf10r1VNjacfxCCXXsvqVPIC4glq3a3OjmWIjGoQ1FwNr+t9Uv7davV/0eUEAWUmunG813PwLSvNEkp6+EJGAUu1f3z8qKPuT7/3gKuGg88ny/YY2Gu2MQhVOEgpOtzhm/ctRtLXxPYoHJ5PfnN+C0VfnfO9j0Y6DE2fwlWoG9gKdPkvMiMc= chorboon@fedora"
+
+
+# resource "aws_s3_bucket" "lb_logs" {
+#     bucket = "lb-logs"
 # }
 
+# resource "aws_s3_bucket_acl" "lb_logs_bucket_acl" {
+#   bucket = aws_s3_bucket.lb_logs.id
+#   acl    = "private"
+# }
 resource "aws_lb" "front_end" {
-  name               = "front-end"
+  name               = "frontend-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.web_subnet-1.id, aws_subnet.web_subnet-2.id, aws_subnet.web_subnet-3.id]
-
+#  subnets            = [aws_subnet.public_subnet-1.id,aws_subnet.public_subnet-2.id]
   enable_deletion_protection = false
 
-  #access_logs {
-  #  bucket  = aws_s3_bucket.lb_logs.id
-  #  prefix  = "test-lb"
-  #  enabled = true
-  #}
+  subnet_mapping {
+    subnet_id     = aws_subnet.public_subnet-1.id
+    allocation_id = aws_eip.frontend-lb-1.id
+  }
+
+  subnet_mapping {
+    subnet_id     = aws_subnet.public_subnet-2.id
+    allocation_id = aws_eip.frontend-lb-2.id
+  }
+#   access_logs {
+#     bucket  = aws_s3_bucket.lb_logs.id
+#     prefix  = "test-lb"
+#     enabled = true
+#   }
 
   #tags = {
   #  Environment = "production"
   #}
 }
 
-resource "aws_lb_target_group" "front_end" {
-  name     = "front-end-tg"
+resource "aws_lb_target_group" "frontend" {
+  name     = "frontend"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.HelloWorld_vpc_1.id
 }
-# resource "aws_lb_target_group_attachment" "front_end" {
-#   target_group_arn = aws_lb_target_group.front_end.arn
-#   target_id        = aws_instance.hello_world-1.id
-#   port             = 80
-# }
+#  resource "aws_lb_target_group_attachment" "front_end" {
+#    target_group_arn = aws_lb_target_group.front_end.arn
+#    target_id        = aws_instance.hello_world-1.id
+#    port             = 80
+#  }
 
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.web-asg.id
+  lb_target_group_arn    = aws_lb_target_group.frontend.arn
+}
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.front_end.arn
   port              = "80"
@@ -204,7 +235,7 @@ resource "aws_lb_listener" "front_end" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
+    target_group_arn = aws_lb_target_group.frontend.arn
   }
 }
 
@@ -224,17 +255,12 @@ resource "aws_launch_configuration" "web" {
   }
 }
 resource "aws_autoscaling_group" "web-asg" {
-  #  availability_zones = ["us-east-1a"]
   desired_capacity     = 1
   max_size             = 1
   min_size             = 1
   launch_configuration = aws_launch_configuration.web.name
   vpc_zone_identifier  = [aws_subnet.web_subnet-1.id, aws_subnet.web_subnet-2.id, aws_subnet.web_subnet-3.id]
-
-  #   launch_template {
-  #     id      = aws_launch_template.web.id
-  #     version = "$Latest"
-  #   }
+#  target_group_arns    = [aws_lb_target_group.frontend.arn]
 }
 
 #scale down at 1800 Singapore Time (1000 UTC)
@@ -260,7 +286,7 @@ resource "aws_autoscaling_schedule" "scale_up" {
 
 resource "aws_autoscaling_attachment" "web" {
   autoscaling_group_name = aws_autoscaling_group.web-asg.id
-  lb_target_group_arn    = aws_lb_target_group.front_end.arn
+  lb_target_group_arn    = aws_lb_target_group.frontend.arn
 }
 # resource "aws_instance" "hello_world-1" {
 #   ami                    = "ami-02f3f602d23f1659d"
